9d905c4dee3f2be835c540848fb61899699c36d4
WindowsAero <fm9396093@126.com>
2022-07-29 18:53:21 +0800

    feat: main features.

---
+++ src/main/kotlin/main.kt
@@ -0,0 +1,45 @@
+import java.io.IOException
+import java.security.MessageDigest
+import java.security.NoSuchAlgorithmException
+import java.util.*
+
+object FinalShell {
+	@Throws(NoSuchAlgorithmException::class, IOException::class)
+	@JvmStatic
+	fun main(args: Array<String>) {
+		print("请输入FinalShell的离线机器码：")
+		val reader = Scanner(System.`in`)
+		val machineCode = reader.nextLine()
+		generateKey(machineCode)
+	}
+
+	@Throws(NoSuchAlgorithmException::class)
+	fun generateKey(hardwareId: String) {
+		val proKey = transform(61305.toString() + hardwareId + 8552)
+		val pfKey = transform(2356.toString() + hardwareId + 13593)
+		println("请将此行复制到离线激活中：")
+		println("高级版: $proKey")
+		println("专业版: $pfKey")
+	}
+
+	@Throws(NoSuchAlgorithmException::class)
+	fun transform(str: String): String {
+//		val md5 = hashMD5(str)
+		return hashMD5(str).substring(8, 24)
+	}
+
+	@Throws(NoSuchAlgorithmException::class)
+	fun hashMD5(str: String): String {
+		val digest = MessageDigest.getInstance("MD5")
+		val hashed = digest.digest(str.toByteArray())
+		val sb = StringBuilder()
+		for (b in hashed) {
+			val len = b.toInt() and 0xFF
+			if (len < 16) {
+				sb.append("0")
+			}
+			sb.append(Integer.toHexString(len))
+		}
+		return sb.toString()
+	}
+}
